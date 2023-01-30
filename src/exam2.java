b332cf8a7fa31f5ad4f6905f50da36268e207b29
eunji <qkrdmswl1018@naver.com>
2023-01-30 23:12:02 +0900

    나이 출력

---
+++ src/exam2.java
@@ -0,0 +1,11 @@
+import java.time.LocalDate;
+
+class Solution {
+    public int solution(int age) {
+        // LocalDate today = LocalDate.now();
+        // int year = today.getYear() - 1;
+        // int answer = year - age + 1;
+        int answer = 2022 - age + 1;
+        return answer;
+    }
+}
