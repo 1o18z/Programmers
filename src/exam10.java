b3fcc7bec509a975fb8d0191b9f73474a61a9a7f
eunji <qkrdmswl1018@naver.com>
2023-01-31 12:53:32 +0900

    삼각형의 완성 조건(1)

--- src/exam10.java
+++ src/exam10.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int[] sides) {
         int answer = 0;
-        for(int i=0; i<sides.length; i++){
+        for(int i=0; i<2; i++){
             if(sides[0] > sides[1] && sides[0] > sides[2]){
                 if(sides[0] < sides[1] + sides[2]){
                     answer = 1;
@@ -18,6 +18,16 @@ class Solution {
                 if(sides[2] < sides[1] + sides[0]){
                     answer = 1;
                 }else{
+                    answer = 2;
+                }
+            } else{
+                if(sides[0] < sides[1] + sides[2]){
+                    answer = 1;
+                }else if(sides[1] < sides[0] + sides[2]){
+                    answer = 1;
+                }else if(sides[2] < sides[1] + sides[0]){
+                    answer = 1;
+                } else{
                     answer = 2;
                 }
             }
