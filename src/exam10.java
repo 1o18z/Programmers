4a4556e5dcf7100aa956ff19b99dfd39627655a4
eunji <qkrdmswl1018@naver.com>
2023-01-31 12:47:26 +0900

    삼각형의 완성 조건(1)

---
+++ src/exam10.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solution(int[] sides) {
+        int answer = 0;
+        for(int i=0; i<sides.length; i++){
+            if(sides[0] > sides[1] && sides[0] > sides[2]){
+                if(sides[0] < sides[1] + sides[2]){
+                    answer = 1;
+                }else{
+                    answer = 2;
+                }
+            } else if(sides[1] > sides[0] && sides[1] > sides[2]){
+                if(sides[1] < sides[0] + sides[2]){
+                    answer = 1;
+                }else{
+                    answer = 2;
+                }
+            } else if(sides[2] > sides[0] && sides[2] > sides[1]){
+                if(sides[2] < sides[1] + sides[0]){
+                    answer = 1;
+                }else{
+                    answer = 2;
+                }
+            }
+        }
+        return answer;
+    }
+}
