8d509d8f5f2fd8c94886dda17a75a68bb6da1dd4
eunji <qkrdmswl1018@naver.com>
2023-02-03 22:49:14 +0900

    숨어있는 숫자의 덧셈 (1)

--- src/exam21.java
+++ src/exam21.java
@@ -1,7 +1,13 @@
 package src;
 
 public class exam21 {
-  public static void main(Stringp[] args){
-
+  public int solution(String my_string) {
+    int answer = 0;
+    String mst = my_string.toLowerCase().replaceAll("[abcdefghijklmnopqrstudvwxyz]", "");
+    String[] arr = mst.split("");
+    for (int i = 0; i < arr.length; i++) {
+      answer += Integer.parseInt(arr[i]);
+    }
+    return answer;
   }
 }
