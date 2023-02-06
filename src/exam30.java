4a09ef87ad7e505414d8751ea93a2b11ded78315
eunji <qkrdmswl1018@naver.com>
2023-02-06 10:19:20 +0900

    문자열 정렬하기 (1)

--- src/exam30.java
+++ src/exam30.java
@@ -4,9 +4,14 @@ public class exam30 {
 
   public int[] solution(String my_string) {
     return Arrays.stream(my_string.split(""))
+                        // 받은 문자열을 한 글자씩 나누고
             .filter(i -> i.matches("[0-9]"))
+                // 나눈 문자열 중 0~9인 문자만 남기고
             .mapToInt(Integer::parseInt)
+                // integer로 변환 후
             .sorted()
+                // 정렬하고
             .toArray();
+                // 배열로 변환
   }
 }
