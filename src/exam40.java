90d289b8e24f5ac3dc57049ffb4ce165bee00b4c
eunji <qkrdmswl1018@naver.com>
2023-02-09 23:44:55 +0900

    영어가 싫어요

---
+++ src/exam40.java
@@ -0,0 +1,6 @@
+public class exam40 {
+  public long solution(String numbers) {
+    return Long.parseLong(numbers.replaceAll("zero", "0").replaceAll("one", "1").replaceAll("two", "2").replaceAll("three", "3").replaceAll("four", "4").replaceAll("five", "5").replaceAll("six", "6").replaceAll("seven", "7").replaceAll("eight", "8").replaceAll("nine", "9"));
+  }
+}
+
