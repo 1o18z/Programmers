c58917bebc8b94709db3c4aaf030eae4c8984edc
eunji <qkrdmswl1018@naver.com>
2023-02-16 18:31:53 +0900

    음양 더하기

---
+++ src/exam46.java
@@ -0,0 +1,18 @@
+class exam46 {
+  public int solution(int[] absolutes, boolean[] signs) {
+    int answer = 123456789;
+    int[] arr = new int[signs.length];
+    for (int i = 0; i < absolutes.length; i++) {
+      if (signs[i] == false) {
+        arr[i] = 0 - absolutes[i];
+      } else {
+        arr[i] = absolutes[i];
+      }
+    }
+    int sum = 0;
+    for (int i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+    return sum;
+  }
+}
