73db2744c8d76223c4f1915d5b30b2aa223c946d
eunji <qkrdmswl1018@naver.com>
2023-02-18 00:01:24 +0900

    수박수박수박수박수박수?

--- src/exam48.java
+++ src/exam48.java
@@ -1,18 +1,13 @@
 class exam48 {
-  public int solution(int[] absolutes, boolean[] signs) {
-    int answer = 123456789;
-    int[] arr = new int[signs.length];
-    for(int i=0; i<absolutes.length; i++){
-      if(signs[i] == false){
-        arr[i] = 0 - absolutes[i];
-      } else{
-        arr[i] = absolutes[i];
+  public String solution(int n) {
+    String answer = "";
+    for (int i = 1; i <= n; i++) {
+      if (i % 2 != 0) {
+        answer += "수";
+      } else {
+        answer += "박";
       }
     }
-    int sum = 0;
-    for(int i=0; i<arr.length; i++){
-      sum += arr[i];
-    }
-    return sum;
+    return answer;
   }
 }
