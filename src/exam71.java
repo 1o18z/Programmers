851c3156469c40dd51c1fd1b22bbc0a03c0b42d7
eunji <qkrdmswl1018@naver.com>
2023-03-08 10:36:13 +0900

    완주하지 못한 선수

--- src/exam71.java
+++ src/exam71.java
@@ -1,21 +1,17 @@
 import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
-        List<String> list = new ArrayList<>();
+        List<String> list = new LinkedList<>(Arrays.asList(participant));
 
         Collections.sort(list);
         Arrays.sort(completion);
-
-        for(int i=0; i<participant.length; i++){
-            list.add(participant[i]);
-        }
+        
         for(int i=0; i<completion.length; i++){
-            if(list.contains(completion[i])){
+            if(list.contains(completioåçn[i])){
                 list.remove(completion[i]);
             }
         }
-        answer += list.get(0);
-        return answer;
+        return list.get(0);
     }
 }
+// 효율성 테스트 통과!!
