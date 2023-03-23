026573f36f457987608dd7bab06fb9b1c25b7cf3
eunji <qkrdmswl1018@naver.com>
2023-03-23 19:44:01 +0900

    완주하지 못한 선수

--- src/exam71.java
+++ src/exam71.java
@@ -1,17 +1,22 @@
 import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        List<String> list = new LinkedList<>(Arrays.asList(participant));
-
-        Collections.sort(list);
-        Arrays.sort(completion);
+        String answer = "";
         
-        for(int i=0; i<completion.length; i++){
-            if(list.contains(completioåçn[i])){
-                list.remove(completion[i]);
+        Map<String, Integer> map = new HashMap<>();
+        
+        for(String p : participant){
+            map.put(p, map.getOrDefault(p, 0) + 1);
+        } // map에 참여자 넣을 건데 있으면 value +1하고 없으면 key,0
+        for(String c : completion){
+            map.put(c, map.get(c)-1);
+        }
+        
+        for(String m : map.keySet()){
+            if(map.get(m) != 0){
+                answer = m;
             }
         }
-        return list.get(0);
+        return answer;
     }
 }
-// 효율성 테스트 통과!!
