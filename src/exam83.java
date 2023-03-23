15170f394b46cd0ee5eb25ecadc002bc5fc2897c
eunji <qkrdmswl1018@naver.com>
2023-03-23 21:45:04 +0900

    위장

---
+++ src/exam83.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        Map<String, Integer> map = new HashMap<>();
+        
+        for(int i=0; i<clothes.length; i++){
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1],1) + 1);
+        } 
+        for(String key : map.keySet()){
+            answer *= map.get(key);
+        }
+        return answer-1;
+    }
+}
