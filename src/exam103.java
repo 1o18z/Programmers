ad3fb4a26f637f95dba7906d4650a7537cc35e5c
eunji <qkrdmswl1018@naver.com>
2023-05-18 19:38:53 +0900

    주식 가격

--- src/exam103.java
+++ src/exam103.java
@@ -1,15 +1,19 @@
+import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
         int[] result = new int[prices.length];
+        Stack<Integer[]> stack = new Stack<>();
         
         for(int i=0; i<prices.length; i++){
-            for(int j=i+1; j<prices.length; j++){
-                result[i]++;
-                if(prices[i] > prices[j]){
-                    break;
-                }
+            result[i] = result.length -1 -i;
+            Integer[] arr = {i, prices[i]};
+            
+            while(!stack.empty() && stack.peek()[1] > prices[i]){
+                Integer[] price = stack.pop();
+                result[price[0]] = i - price[0];
             }
-        }
+            stack.push(arr);
+            }
         return result;
     }
 }
