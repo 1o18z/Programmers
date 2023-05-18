db532bafee9c32266d4156654c90ee3310c8749e
eunji <qkrdmswl1018@naver.com>
2023-05-18 17:52:07 +0900

    주식 가격

---
+++ src/exam103.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] result = new int[prices.length];
+        
+        for(int i=0; i<prices.length; i++){
+            for(int j=i+1; j<prices.length; j++){
+                result[i]++;
+                if(prices[i] > prices[j]){
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
