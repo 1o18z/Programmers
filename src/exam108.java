9275aeeee5e1ab65604ff1ef47f2b0c3c6b69d81
eunji <qkrdmswl1018@naver.com>
2023-05-29 23:45:01 +0900

    숫자 게임

--- src/exam108.java
+++ src/exam108.java
@@ -1,16 +1,22 @@
-import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Collections;
 class Solution {
     public int solution(int[] A, int[] B) {
-        int count = 0;
-        Arrays.sort(A);
-        Arrays.sort(B);
-        
-        for(int i=0; i<A.length; i++){
-            if(A[count] >= B[i]){
-                continue;
-            }
-            count++;
+        int sum=0;
+        PriorityQueue<Integer> queA = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> queB = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int i=0; i<A.length; i++) {
+	        queA.offer(A[i]);
+	        queB.offer(B[i]);
         }
-        return count;
+
+        while (!queA.isEmpty()) {
+	        if (queA.poll() < queB.peek()) {
+		        queB.poll();
+		        sum++;
+	        }
+        }
+        return sum;
     }
 }
