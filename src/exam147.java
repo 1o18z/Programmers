05ac094e8423af106c0d2b10ed41211e7aaec563
박은지 <qkrdmswl1018@naver.com>
2024-01-02 23:45:20 +0900

    로또의 최고 순위와 최저 순위

---
+++ src/exam147.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] solution(int[] lottos, int[] win_nums) {
+        Map<Integer, Boolean> map = new HashMap<Integer, Boolean>();
+        int zeroCount = 0;
+
+        for(int lotto : lottos) {
+            if(lotto == 0) {
+                zeroCount++;
+                continue;
+            }
+            map.put(lotto, true);
+        }
+
+
+        int sameCount = 0;
+        for(int winNum : win_nums) {
+            if(map.containsKey(winNum)) sameCount++;
+        }
+
+        int maxRank = 7 - (sameCount + zeroCount);
+        int minRank = 7 - sameCount;
+      
+        if(maxRank > 6) maxRank = 6;
+        if(minRank > 6) minRank = 6;
+
+        return new int[] {maxRank, minRank};
+    }
+}
